6c08531c83a5fc35acd301f9aebc68c8f1c499d0
Reigenleif <16721237@mahasiswa.itb.ac.id>
2026-07-10 11:08:44 +0700

    fix: wrapper name

--- utils/wrapper/__init__.py
+++ utils/wrapper/__init__.py
@@ -1,5 +1,6 @@
 from .dual_seq_cmdonly import DualSeqCMDOnlyWrapper
 from .dual_seq_wrapper import DualSeqWrapper
 from .pretrain_wrapper import PretrainWrapper
+from .grpo_wrapper import GRPOWrapper
 
-__all__ = ["DualSeqCMDOnlyWrapper", "DualSeqWrapper", "PretrainWrapper"]
+__all__ = ["DualSeqCMDOnlyWrapper", "DualSeqWrapper", "PretrainWrapper", "GRPOWrapper"]
