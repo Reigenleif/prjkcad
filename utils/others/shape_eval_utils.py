9d1fa85443bca30aa25a989da555df85cf37e5c6
Reigenleif <16721237@mahasiswa.itb.ac.id>
2026-07-20 23:42:08 +0700

    add: DualSeq examples

--- utils/others/shape_eval_utils.py
+++ utils/others/shape_eval_utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -6,7 +8,6 @@ from utils.render import render_to_image, render_dual_seq_to_shape
 from utils.evaluate import chamfer_distance_from_shapes
 from utils.dual_seq import DualSeq
 
-# Example Shapes
 def make_cube(size=1.0):
     cmds = [
         "COOR", "FACE", "LOOP",
@@ -62,8 +63,6 @@ def make_cylinder(r=0.5, h=1.0):
     ]
     return render_dual_seq_to_shape(cmds, args)
 
-
-# Viz tools
 def visualize_pair(shape1, shape2, name1, name2, out_dir="out/visualizations"):
     os.makedirs(out_dir, exist_ok=True)
     path1 = os.path.join(out_dir, f"{name1}.png")
@@ -89,3 +88,77 @@ def visualize_pair(shape1, shape2, name1, name2, out_dir="out/visualizations"):
     plt.suptitle(f"Chamfer Distance: {cd:.6f}", fontsize=14, fontweight='bold')
     plt.tight_layout()
     plt.show()
+
+def make_cube_dualseq(size=1.0) -> DualSeq:
+    cmds = [
+        "COOR", "FACE", "LOOP",
+        "LINE", "LINE", "LINE", "LINE",
+        "EXTRUDE_NEW"
+    ]
+    args = [
+        {'coor_euax': 0.0, 'coor_euay': 0.0, 'coor_euaz': 0.0, 'coor_tx': 0.0, 'coor_ty': 0.0, 'coor_tz': 0.0},
+        {}, {},
+        {'line_sx': 0.0, 'line_sy': 0.0, 'line_ex': size, 'line_ey': 0.0},
+        {'line_sx': size, 'line_sy': 0.0, 'line_ex': size, 'line_ey': size},
+        {'line_sx': size, 'line_sy': size, 'line_ex': 0.0, 'line_ey': size},
+        {'line_sx': 0.0, 'line_sy': size, 'line_ex': 0.0, 'line_ey': 0.0},
+        {'extrude_new_dtn': size, 'extrude_new_don': 0.0, 'extrude_new_scale': 1.0}
+    ]
+    return DualSeq(cmds=cmds, args=args, uid="cube")
+
+def make_cylinder_dualseq(r=0.5, h=1.0) -> DualSeq:
+    cmds = [
+        "COOR", "FACE", "LOOP",
+        "CIRCLE",
+        "EXTRUDE_NEW"
+    ]
+    args = [
+        {'coor_euax': 0.0, 'coor_euay': 0.0, 'coor_euaz': 0.0, 'coor_tx': 0.0, 'coor_ty': 0.0, 'coor_tz': 0.0},
+        {}, {},
+        {'circle_cx': 0.0, 'circle_cy': 0.0, 'circle_r': r},
+        {'extrude_new_dtn': h, 'extrude_new_don': 0.0, 'extrude_new_scale': 1.0}
+    ]
+    return DualSeq(cmds=cmds, args=args, uid="cylinder")
+
+def make_cylinder_on_top_dualseq(cube_size=1.0, cyl_r=0.3, cyl_h=0.5) -> DualSeq:
+    cmds = [
+        "COOR", "FACE", "LOOP",
+        "LINE", "LINE", "LINE", "LINE",
+        "EXTRUDE_NEW",
+        "COOR", "FACE", "LOOP",
+        "CIRCLE",
+        "EXTRUDE_JOIN"
+    ]
+    args = [
+        {'coor_euax': 0.0, 'coor_euay': 0.0, 'coor_euaz': 0.0, 'coor_tx': 0.0, 'coor_ty': 0.0, 'coor_tz': 0.0},
+        {}, {},
+        {'line_sx': 0.0, 'line_sy': 0.0, 'line_ex': cube_size, 'line_ey': 0.0},
+        {'line_sx': cube_size, 'line_sy': 0.0, 'line_ex': cube_size, 'line_ey': cube_size},
+        {'line_sx': cube_size, 'line_sy': cube_size, 'line_ex': 0.0, 'line_ey': cube_size},
+        {'line_sx': 0.0, 'line_sy': cube_size, 'line_ex': 0.0, 'line_ey': 0.0},
+        {'extrude_new_dtn': cube_size, 'extrude_new_don': 0.0, 'extrude_new_scale': 1.0},
+        {'coor_euax': 0.0, 'coor_euay': 0.0, 'coor_euaz': 0.0, 'coor_tx': 0.0, 'coor_ty': 0.0, 'coor_tz': cube_size},
+        {}, {},
+        {'circle_cx': cube_size / 2.0, 'circle_cy': cube_size / 2.0, 'circle_r': cyl_r},
+        {'extrude_join_dtn': cyl_h, 'extrude_join_don': 0.0, 'extrude_join_scale': 1.0}
+    ]
+    return DualSeq(cmds=cmds, args=args, uid="cylinder_on_top")
+
+def visualize_basic_shapes_with_representations(out_dir="out/visualiations"):
+    from utils.render import render_dual_seq_with_representations_to_img
+    from utils.representations.dual_seq.dual_seq import DualSeqMetadata
+    
+    cube_ds = make_cube_dualseq()
+    cyl_ds = make_cylinder_dualseq()
+    cot_ds = make_cylinder_on_top_dualseq()
+    
+    metadata = DualSeqMetadata()
+    metadata.fit([cube_ds, cyl_ds, cot_ds])
+    
+    os.makedirs(out_dir, exist_ok=True)
+    
+    render_dual_seq_with_representations_to_img(cube_ds, os.path.join(out_dir, "cube_representations.png"), metadata)
+    render_dual_seq_with_representations_to_img(cyl_ds, os.path.join(out_dir, "cylinder_representations.png"), metadata)
+    render_dual_seq_with_representations_to_img(cot_ds, os.path.join(out_dir, "cylinder_on_top_representations.png"), metadata)
+    
+    print(f"Rendered basic shapes with representations to {out_dir}")
